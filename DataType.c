7a24a8b9ca0bd14515fa3a63ce7f585eb39adb02
ChellaMuthu <chellamuthupalanichamy8192@gmail.com>
2021-05-08 13:35:21 -0400

    Data type in c

---
+++ DataType.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a=10;
+char c = 'c';
+float f= 1.56f;
+double d = 45.78;
+printf("\nint=%d",a);
+printf("\nchar=%c",c);
+printf("\nfloat=%.2f",f);
+printf("\ndouble=%.2lf",d);
+}
